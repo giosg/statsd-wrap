128040364d0712b61a8f6481a183ed8e3ba6b56b
Arsi Halttunen <arsi@giosg.com>
2019-01-17 14:38:23 +0000

    Added py.typed file for PEP 561 compliance

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='statsd-wrap',
-    version='0.1.0',
+    version='0.2.0',
     description='Wrapper for Datadog StatsD client.',
     author='giosg',
     author_email='developers@giosg.com',
